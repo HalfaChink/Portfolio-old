2fc5467c5907e443e3de68b487778d4e686d168d
Halfa <85602339+HalfaChink@users.noreply.github.com>
2022-08-28 15:40:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+var current = null;
+document.querySelector('#username').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: 0,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+document.querySelector('#password').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -336,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+document.querySelector('#submit').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -730,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '530 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+
+function login() {
+    let username = document.getElementById('formlogin').username.value;
+    let password = document.getElementById('formlogin').password.value;
+    let Result = document.getElementById('wrong');
+
+    if (username == 'xrpl1' && password == '123') {
+        window.open("homepage.html", "_self");
+    } else if (username == 'user1' && password == '123') {
+        window.open("homepage.html", "_self");
+    } else {
+        Result.innerHTML = 'Username atau Password salah';
+    }
+}
